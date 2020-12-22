cfdf68303427363b62487b9b3ddd913849446605
Miguel Ângelo <miguelcg958@gmail.com>
2020-12-21 22:02:49 -0300

    chore: Added SEO tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,6 +36,44 @@ export default class MyDocument extends Document {
         <Head>
           <link href="./favicon.ico" rel="shortcut icon" type="image/x-icon" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu&display=swap" rel="stylesheet" />
+
+          {/* SEO */}
+          {/* Tags comentadas eu não sei como funcionam :) */}
+          <meta name="description" content="Site feito para a comunidade de programação Dinastia Foca" />
+          <meta name="author" content="Miguel Ângelo" />
+          <meta name="keywords" content="programação, diversão, comunidade, discord, c#, c++, js, javascript, ts, typescript, HTML, CSS, reactjs, nextsjs" />
+
+          {/* <link rel="alternate" href="" hrefLang="pt-br" /> */}
+          <meta name="robots" content="index, follow" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+          <meta property="og:url" content="https://github.com/foca-enterprises" />
+          <meta property="og:title" content="Foca Enterprises" />
+          <meta property="og:image" content="https://avatars0.githubusercontent.com/u/69772552?v=4" />
+          <meta property="og:description" content="Comunidade de programação" />
+          <meta name="theme-color" content="#34CB79" />
+
+          {/* <meta property="business:contact_data:country_name" content="PAÍS" /> */}
+          {/* <meta property="business:contact_data:website" content="URL" /> */}
+          {/* <meta property="business:contact_data:region" content="SP" /> */}
+          {/* <meta property="business:contact_data:email" content="EMAIL" /> */}
+          {/* <meta property="business:contact_data:phone_number" content="TEL" /> */}
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:description" content="Comunidade de programação" />
+          <meta name="twitter:title" content="Foca Enterprises" />
+          <meta name="twitter:image" content="https://avatars0.githubusercontent.com/u/69772552?v=4" />
+          <meta name="twitter:creator" content="@guel5g" />
+          <meta name="twitter:site" content="@guel5g" />
+
+          <meta name="geo.placename" content="Brasil" />
+          <meta name="geo.region" content="BR" />
+          <link rel="canonical" href="https://foca-site.vercel.app/" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta name="format-detection" content="telephone=no" />
+          {/* SEO */}
         </Head>
 
         <body>
