f390d4dd3597a3e57cdc198f75483818e7ecb8c0
Miguel Ângelo <miguelcg958@gmail.com>
2020-12-21 23:26:30 -0300

    chore: Simple color changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,8 @@ export default class MyDocument extends Document {
 
           {/* SEO */}
           {/* Tags comentadas eu não sei como funcionam :) */}
+          <meta name="google-site-verification" content="cqR-9eTLBmnbh0XpDXQQLZpzqUzCXFie71s6ohl1HsI" />
+
           <meta name="description" content="Site feito para a comunidade de programação Dinastia Foca" />
           <meta name="author" content="Miguel Ângelo" />
           <meta name="keywords" content="programação, diversão, comunidade, discord, c#, c++, js, javascript, ts, typescript, HTML, CSS, reactjs, nextsjs" />
@@ -46,10 +48,9 @@ export default class MyDocument extends Document {
           {/* <link rel="alternate" href="" hrefLang="pt-br" /> */}
           <meta name="robots" content="index, follow" />
 
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <meta property="og:url" content="https://github.com/foca-enterprises" />
           <meta property="og:title" content="Foca Enterprises" />
-          <meta property="og:image" content="https://avatars0.githubusercontent.com/u/69772552?v=4" />
+          <meta property="og:image" content="https://foca-site.vercel.app/foca_image.png" />
           <meta property="og:description" content="Comunidade de programação" />
           <meta name="theme-color" content="#34CB79" />
 
@@ -62,7 +63,7 @@ export default class MyDocument extends Document {
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:description" content="Comunidade de programação" />
           <meta name="twitter:title" content="Foca Enterprises" />
-          <meta name="twitter:image" content="https://avatars0.githubusercontent.com/u/69772552?v=4" />
+          <meta name="twitter:image" content="https://foca-site.vercel.app/foca_image.png" />
           <meta name="twitter:creator" content="@guel5g" />
           <meta name="twitter:site" content="@guel5g" />
 
